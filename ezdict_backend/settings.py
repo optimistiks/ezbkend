013cbb379ef09b7b9ecd0f8dfca0e5d1ca3848f5
Potapov Max <mp@multiship.ru>
2014-10-10 17:03:05 +0400

    change db settings

--- ezdict_backend/settings.py
+++ ezdict_backend/settings.py
@@ -18,8 +18,8 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'ezdict',
         'PASSWORD': 'ezdict',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': 'ezdict-db',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
